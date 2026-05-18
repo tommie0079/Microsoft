ce0ca8dc3b8e27346bea2d75623c60a95ca4d83d
tommie0079 <t.lentjes@hotmail.com>
2026-05-18 13:34:31 +0200

    Update install.ps1

--- PrintDeployIntune/install.ps1
+++ PrintDeployIntune/install.ps1
@@ -1,12 +1,17 @@
 # --- Config ---
 
-$PrinterName = "Printer på kontoret"
+$PrinterName = "Printer pa kontoret"
 $PrinterIP = "192.168.1.52"
 $PortName = "IP_$PrinterIP"
 
-$DriverName = "Samsung Universal Print Driver 3"
-$INFFileName = "us016.inf"
-$INFPath =  "$PSScriptRoot\UPD\$INFFileName"
+# Change these two values when you replace the driver package in UPD.
+$DriverName = "HP Universal Printing PS"
+$INFFileName = "hpcu355z.inf"
+$INFPath = "$PSScriptRoot\UPD\$INFFileName"
+
+if (-not (Test-Path $INFPath)) {
+	throw "Driver INF not found: $INFPath"
+}
 
 # --- Install printer ---
 
@@ -17,3 +22,4 @@ Add-PrinterPort -Name $PortName -PrinterHostAddress $PrinterIP
 
 # Installer printer
 Add-Printer -Name $PrinterName -DriverName $DriverName -PortName $PortName
+
