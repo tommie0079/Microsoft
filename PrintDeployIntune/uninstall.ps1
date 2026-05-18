f1d0e966789b24d334c5ec1bad520637371b45e1
tommie0079 <t.lentjes@hotmail.com>
2026-05-18 13:34:59 +0200

    Update uninstall.ps1

--- PrintDeployIntune/uninstall.ps1
+++ PrintDeployIntune/uninstall.ps1
@@ -1,12 +1,8 @@
 # --- Config ---
 
-$PrinterName = "Printer på kontoret"
+$PrinterName = "Printer pa kontoret"
 $PrinterIP = "192.168.1.52"
 $PortName = "IP_$PrinterIP"
-
-$DriverName = "Samsung Universal Print Driver 3"
-$INFFileName = "us016.inf"
-$INFPath =  "$PSScriptRoot\UPD\$INFFileName"
 
 # Avinstaller printer og printerport
 if (Get-Printer -Name $PrinterName -ErrorAction SilentlyContinue) {
