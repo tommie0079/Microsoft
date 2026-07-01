c9904e01c2362319ab8669e03a9b0e8883c2c67d
tommie lentjes <tommie.lentjes@vlfk.no>
2026-07-01 10:55:43 +0200

    asd

--- Intune/Printer Deployment with Intune/1. Package-Printer.bat Guide/Files/detection.ps1
+++
@@ -1,7 +0,0 @@
-$PrinterName = "Printer 3. etg"
-if (Get-Printer -Name $PrinterName -ErrorAction SilentlyContinue) {
-    Write-Output "Printer Detected"
-    Exit 0
-} else {
-    Exit 1
-}
--- Intune/WireGuard VPN – Automated Deployment via Intune/detection.ps1
+++
@@ -1,9 +0,0 @@
-$wireGuardExe = "C:\Program Files\WireGuard\wireguard.exe"
-$configPath = "C:\ProgramData\WireGuard\CorporateVPN.conf"
-
-if ((Test-Path $wireGuardExe) -and (Test-Path $configPath)) {
-    Write-Output "Detected"
-    exit 0
-}
-
-exit 1
